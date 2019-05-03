d613ae7b19eb0adbf7b9a4d12c7aac7cab1742ca
Micaella <micaella2212@gmail.com>
2019-05-03 15:54:56 -0300

    Adding user intents
    Co-authored-by: Eduardo Lima <eduardolima.df@gmail.com>

--- bot/actions/action_user.py
+++ bot/actions/action_user.py
@@ -10,23 +10,21 @@ class User_Action(Action):
 
     def run(self, dispatcher, tracker, domain):
 
-        tracker_state = tracker.current_state()
-        sender_id = tracker_state['sender_id']
+    tracker_state = tracker.current_state()
+    sender_id = tracker_state['sender_id']
+    user_local = tracker.get_slot('user_local')
+    user_sport = tracker.get_slot('user_sport')
+    user_day = tracker.get_slot('user_day')
+    user_time = tracker.get_slot('user_time')
 
-        user_local = tracker.get_slot('user_local')
-        user_sport = tracker.get_slot('user_sport')
-        user_day = tracker.get_slot('user_day')
-        user_time = tracker.get_slot('user_time')
-
-        dataJson = {	
-  "telegramId": sender_id,
-	"sport": user_sport,
-	"notificationDays": user_day,
-	"notificationTime": user_time,
-	"local": user_local
+    dataJson = {	
+        "telegramId": sender_id,
+	    "sport": user_sport,
+	    "notificationDays": user_day,
+	    "notificationTime": user_time,
+	    "local": user_local
         }
 
-        response = requests.post(
-    'localhost:3003/registerUser', data = dataJson)
+    response = requests.post('localhost:3003/registerUser', data = dataJson)
 
 
